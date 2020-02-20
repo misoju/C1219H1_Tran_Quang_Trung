7999b0161310aa47008c934b7a4cc83d467bcaeb
misoju <trantrung0195@gmail.com>
2020-02-20 20:14:03 +0700

    first commit

---
+++ Module2/src/NgonNguLapTrinhJava/B1_JavaOverView/BaiTap/HienThiLoiChao.java
@@ -0,0 +1,13 @@
+package NgonNguLapTrinhJava.B1_JavaOverView.BaiTap;
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args){
+        //nhap ten
+        Scanner scname = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name1 = scname.nextLine();
+        //in ten
+        System.out.println("Hello: " + name1);
+    }
+}
---
+++ Module2/src/NgonNguLapTrinhJava/B1_JavaOverView/ThucHanh/B1_JavaOverview.java
@@ -0,0 +1,9 @@
+package NgonNguLapTrinhJava.B1_JavaOverView.ThucHanh;
+
+public class B1_JavaOverview {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world") ;
+	// write your code here
+    }
+}
